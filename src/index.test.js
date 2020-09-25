8f54b1a56606426263c96ff8e2efd82233476392
Marcin Kaminski <marcin.kaminski@schibsted.com>
2020-09-25 11:59:37 +0200

    ✨ Added CI, Builds etc. workflows and fixed configuration

---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+describe('index', () => {
+  it('initial test', () => {
+    expect(true).toBe(true);
+  });
+});
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,37 +2,41 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const pkg = require('./package.json');
+const package_ = require('./package.json');
 
 module.exports = {
   mode: 'production',
   entry: {
-    index: path.join(__dirname, pkg.main),
-  },
+    index: path.join(__dirname, package_.main),  },
   output: {
-    ecmaVersion: 5,
-    filename: `[name].js`,
+    filename: '[name].js',
     path: path.join(__dirname, 'dist'),
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({ chunks: ['index'], template: 'src/index.html' }),
     new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false })
   ],
   module: {
     rules: [
       {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false
+        }
+      },
+      {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', { loader: 'clean-css-loader', options: { level: 2, compatibility: 'ie7' }}],
+        use: ['style-loader', 'css-loader', { loader: 'clean-css-loader', options: { level: 2 } }],
       },
       {
         test: /\.svg/,
         use: {
           loader: 'svg-url-loader',
-          options: { iesafe: true, }
-        }
+          options: { iesafe: true },
+        },
       },
       {
         test: /\.m?js$/,
@@ -49,8 +53,8 @@ module.exports = {
   optimization: {
     minimize: true,
     minimizer: [
-      new TerserPlugin({terserOptions: { ecma: 5, safari10: true, ie8: true }),
       new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
     ],
     splitChunks: {
       chunks: 'all',
