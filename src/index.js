e91449db69b365518fbab3155fe782cd25d5fd8a
Marcin Kaminski <marcin.kaminski@schibsted.com>
2020-09-25 12:30:30 +0200

    ✨ Added husky

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable filenames/no-index */
+
+(() => {
+  // eslint-disable-next-line no-console
+  console.error(2 + 2);
+})();
