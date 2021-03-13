fa68eec93e9c0c6248df5c207383311319dd64d1
Marcin <ma.kaminski+github@icloud.com>
2021-03-13 14:26:42 +0100

    Remove IP tracking

--- src/api/geo.js
+++ src/api/geo.js
@@ -2,6 +2,6 @@ const BASE_URL = 'https://freegeoip.app/json/';
 
 export const getGeo = async () => {
   const response = await fetch(BASE_URL);
-  const { country_name: country, city, ip } = await response.json();
-  return { country, city, ip };
+  const { country_name: country, city } = await response.json();
+  return { country, city };
 };
